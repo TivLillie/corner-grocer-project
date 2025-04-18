b94f793675c281e20bbca263c84db697c60dc328
Tivoli Lillie <Tivoli.Lillie@snhu.edu>
2025-04-18 18:15:44 -0400

    First commit

---
+++ driver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main () {
+    std::vector<std::string> menuOptions = {"[1] Find the purchase frequency of an item"
+                                            "[2] Print a frequency list for all items purchased"
+                                            "[3] Print a historgram for all items purchased"
+                                            "[4] Exit"
+                                            };
+
+
+
+    return 0;
+}
